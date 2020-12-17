4ce0041678291e6e798d156fb754ccbd51038233
stevekutz <stkutz@gmail.com>
2020-12-17 15:35:02 -0600

    the request body is parsed for user input data and saved to a file

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const http = require('http');   // Node's way of importing modules, files
 
 const fs = require('fs')
-//
+
+let count = 0;
+
+// Date is imported by default in Node, import NOT needed
+// return date is MM/DD/YYYY format
+const writeDate_CB = () => {    
+    let dateObj = new Date;
+    let year = dateObj.getFullYear();
+    let month = ('1' + (dateObj.getMonth() + 1)).slice(-2);  // e.g. 03, 10  starts at 0
+    let day = ('0' + dateObj.getDate()).slice(-2);
+
+    let dateStr = month + '/' + day + '/' +  year
+    console.log(dateStr)
+    return dateStr
+}
+
+// createServer contains a `requestListener` function
 const server = http.createServer((req, res) => {
     const {url, method, headers} = req    
+    count += 1
 
-
+    // an inefficient way to write HTML to browser 
+    //    action points to route where request data is sent
+    // form method defines POST request with data sent to router message
+    // form tag looks for other data. From input, any input defined with name attribute gets attached as key-value pairs to body of request
+    // key is name & value is user input
     if (url ==='/') {
         res.write('<html><head><title> First Node Page </title></head>');
-        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"/><button type = "Submit"> Send </button> </body></form>');
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"/><button type = "Submit"> Send </button> </form></body>');
         res.write('</html>');
-        return res.end();
+        // Indicates response message complete. Node send response to client where browser looks at header to know how to render
+        // response.end() MUST be called on each response !!!
+        return res.end(); 
     }
 
     console.log( "Req obj  url, method, & headers section")  
@@ -18,29 +41,37 @@ const server = http.createServer((req, res) => {
     console.log(" \n method => ", method);
     console.log(" \n headers => ", headers);
 
-    // if route matches, writeto file and end
+    // if route matches, write to file and end
     if (url === '/message' && method === 'POST') {
         const body = []
         
+        // `on` is an event listener, looks for `data` event and then runs the listener callback function
+        //  `chunk` is a part of the data(either a string of part of the `Buffer`)
         req.on('data', (chunk) => {
             console.log(chunk)
             body.push(chunk)
         });
+
+        // `on` is an event listener, looks for `end` event and then runs the listener callback function
+        //    event NOT emitted until no more data is available
+        // global Buffer class is used to manage data transfers(e.g. chunks of data from entire amount of data) from a stream of data
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log("parsedBody ",  parsedBody);
-            const message = parsedBody.split('=', 1);
+            const message = parsedBody.split('=')[1]; // split on `=` separator and take value (e.g. index 1)
+            fs.writeFileSync('message.txt', message);  // write input value to file 
         })
 
-        fs.writeFileSync('message.txt', 'DummyData test write');
         res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        res.setHeader('Location', '/');  // writes metadata
+        return res.end(dateStr); // last parameter is passed a CB
+
     }
 
-
+    // without res.end(), this never gets sent and server keeps running
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title> First Node Page </title></head><body><h1> Node Server Running </h1> </body></html>');
+    res.write(`<html><head><title> First Node Page </title></head><body><h1> Node Server Running ${count}</h1> </body></html>`);
+    console.log("\t\t ", count)
     res.end();
 });
 
